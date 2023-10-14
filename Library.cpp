710e8a5cdc9627361ec346ade69f3f00e9a68520
Shahir <101193678+ShahirKhaled@users.noreply.github.com>
2023-10-14 22:38:26 +0430

    User Authentication

--- Library.cpp
+++ Library.cpp
@@ -5,37 +5,46 @@ using namespace std;
 string adminUser = "ShahirKhaled";
 string adminPassword = "Shahir123";
 
-bool authUser(string userName, string password) {
-    string inputUserName;
-    string inputPassword;
-    cout << "Please enter your Username: ";
-    cin >> userName;
-    cout << "Please enter your password: ";
-    cin >> password;
-    bool x = inputUserName == userName;
-    cout << x;
-    if (inputUserName == userName && inputPassword == password) return true;
-    else {
-        cout << "Wrong Input! Please try again!";
-        return false;
+string user = "someone";
+string password = "something";
+
+void authUser(string userName, string password) {
+    int attempts = 5;
+
+    for (int i = 0; i < 1; i++) {
+        string inputUserName;
+        string inputPassword;
+        cout << "Please enter your Username: ";
+        cin >> inputUserName;
+        cout << "Please enter your password: ";
+        cin >> inputPassword;
+        if (inputUserName != userName && inputPassword != password) {
+            attempts--;
+            if (attempts < 1) {
+                cout << "Sorry! You are out of attempts :(";
+                break;
+            }
+            i--;
+            cout << "Wrong Input! Please try again!";
+            cout << "\nNumber of attempts left: " << attempts << "\n\n";
+        }
     }
 }
 
-struct user {
-    string userName;
-    string password;
-};
-
 int main() {
     int currentUser;
-    
+
     cout << "\tWelcome to Shahir's Teams' library! :)\n";
     cout << "Press 1 if you are an admin, 2 if you are a user: ";
     cin >> currentUser;
     
     switch (currentUser) {
         case 1:
-            cout << authUser(adminUser, adminPassword);
+            authUser(adminUser, adminPassword);
+            break;
+            
+        case 2:
+            authUser(user, password);
             break;
     }
 
